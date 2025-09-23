5b3bcdcb53eeeaaee1fb5665f8aa11eb9d5858b4
Yuuki-Nagisa <paul.dibyojit2005@gmail.com>
2025-09-24 01:02:51 +0530

    ..

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,24 +3,34 @@ import { motion, useScroll, useTransform } from "motion/react";
 import logo from "../public/logo.png";
 
 const Home = () => {
-  const { scrollYProgress } = useScroll();
-  const scale = useTransform(scrollYProgress, [0.1, 0.5, 0.8], [0.9, 1.4, 1]);
   return (
     <>
       <HeroComponent />
-      <div className="h-[600px] p-8 text-center flex flex-col items-center ">
-        <motion.div
-          style={{ scale }}
-          className="w-5/6 md:w-1/2 mt-2 text-2xl bg-[#1a1c1e]/70 border-green-500 border-l-3 border-r-4 rounded-3xl flex flex-col p-4 items-center justify-center shadow-lg shadow-blue-500/50 backdrop-blur-md"
-        >
-          <h1 className="text-4xl font-semibold mb-4">
-            Welcome to <span className="text-green-500 font-bold">Saogari</span>
-          </h1>
-          The Photography Club is a vibrant community of students passionate
-          about capturing moments, exploring creative angles, and learning
-          camera skills. Through photo walks, workshops, and exhibitions,
-          members share ideas and showcase their best shots.{" "}
-        </motion.div>
+      <div className="h-[600px] p-20 text-center flex flex-col items-center justify-center bg-black">
+        <h1 className="font-bold text-5xl text-green-500">About Us</h1>
+        <div className="w-30 h-2 bg-green-500 my-6"></div>
+        <div className="flex items-center mt-4 h-full">
+          <motion.div
+            initial={{ opacity: 0.5, y: 50 }}
+            whileInView={{
+              opacity: 1,
+              y: 0,
+              transition: { duration: 0.6, ease: "easeOut" },
+            }}
+            viewport={{ once: true }}
+            whileHover={{ y: -20, scale: 1.02, transition: { duration: 0.3 } }}
+            className="w-5/6 md:w-1/2 mt-2 text-lg md:text-2xl bg-gray-900 rounded-3xl flex flex-col p-4 items-center justify-center shadow-lg shadow-[#1a1c1e] backdrop-blur-md"
+          >
+            <h1 className="text-4xl font-semibold mb-4">
+              Welcome to{" "}
+              <span className="text-green-500 font-bold">Saogari</span>
+            </h1>
+            The Photography Club is a vibrant community of students passionate
+            about capturing moments, exploring creative angles, and learning
+            camera skills. Through photo walks, workshops, and exhibitions,
+            members share ideas and showcase their best shots.{" "}
+          </motion.div>
+        </div>
       </div>
       <div className="h-[600px] w-full bg-[#1a1c1e] flex justify-center p-10">
         <img
